b49070540db49ce5fcf7eb94b2d787252d6a2132
Arthur Chan <arthurgchan@gmail.com>
2020-08-15 00:30:53 -0700

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 return array(
     'authorizationRequestUrl' => 'https://appcenter.intuit.com/connect/oauth2',
     'tokenEndPointUrl' => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
-    'client_id' => 'Enter the clietID from Developer Portal',
-    'client_secret' => 'Enter the clientSecret from Developer Portal',
+    'client_id' => 'ABFaiMduXN3m13fFZVqyd80Q40KDh5DMuLlkrs0BO1qeu1jDwJ',
+    'client_secret' => 'CCPSOesz894tTaJmMJCmdIsmYDdOflN8XcQxoKR5',
     'oauth_scope' => 'com.intuit.quickbooks.accounting openid profile email phone address',
-    'oauth_redirect_uri' => 'http://localhost:3000/callback.php',
+    'oauth_redirect_uri' => 'http://sheetsync.mercintel.com/callback.php',
 )
 ?>
